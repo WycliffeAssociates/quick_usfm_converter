2a048325af106a97bbc481f31d12389ad08443c6
Craig Oliver <craig.robert.oliver@gmail.com>
2019-04-25 13:59:10 -0400

    It all works, more or less

---
+++ Hackathon_Converter/MainForm.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Hackathon_Converter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnConvert = new System.Windows.Forms.Button();
+            this.btn_AddFiles = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.fileDataGrid = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnConvert
+            // 
+            this.BtnConvert.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.BtnConvert.Location = new System.Drawing.Point(0, 87);
+            this.BtnConvert.Name = "BtnConvert";
+            this.BtnConvert.Size = new System.Drawing.Size(800, 70);
+            this.BtnConvert.TabIndex = 0;
+            this.BtnConvert.Text = "Convert";
+            this.BtnConvert.UseVisualStyleBackColor = true;
+            this.BtnConvert.Click += new System.EventHandler(this.OnConvertButtonClick);
+            // 
+            // btn_AddFiles
+            // 
+            this.btn_AddFiles.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_AddFiles.Location = new System.Drawing.Point(0, 0);
+            this.btn_AddFiles.Name = "btn_AddFiles";
+            this.btn_AddFiles.Size = new System.Drawing.Size(800, 70);
+            this.btn_AddFiles.TabIndex = 1;
+            this.btn_AddFiles.Text = "Add Directory";
+            this.btn_AddFiles.UseVisualStyleBackColor = true;
+            this.btn_AddFiles.Click += new System.EventHandler(this.OnAddFilesButtonClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btn_AddFiles);
+            this.panel1.Controls.Add(this.BtnConvert);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 293);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 157);
+            this.panel1.TabIndex = 3;
+            // 
+            // fileDataGrid
+            // 
+            this.fileDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.fileDataGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.fileDataGrid.Location = new System.Drawing.Point(0, 0);
+            this.fileDataGrid.Name = "fileDataGrid";
+            this.fileDataGrid.RowTemplate.Height = 33;
+            this.fileDataGrid.Size = new System.Drawing.Size(800, 293);
+            this.fileDataGrid.TabIndex = 4;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.fileDataGrid);
+            this.Controls.Add(this.panel1);
+            this.Name = "MainForm";
+            this.Text = "Hackathon Converter";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.fileDataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnConvert;
+        private System.Windows.Forms.Button btn_AddFiles;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView fileDataGrid;
+    }
+}
+
---
+++ Hackathon_Converter/MainForm.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hackathon_Converter
+{
+    public partial class MainForm : Form
+    {
+        //private List<String> selectedFiles = new List<string>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            //var bindingList = new BindingList<String>(selectedFiles);
+            //var source = new BindingSource(bindingList, null);
+            //fileDataGrid.DataSource = source;
+
+            fileDataGrid.ColumnCount = 1;
+            fileDataGrid.Columns[0].Name = "File";
+            fileDataGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+        }
+
+
+        private void OnAddFilesButtonClick(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.Description = "Select the directory conatining the files you want to convert.";
+            // Default to the My Documents folder.
+            folderBrowserDialog.RootFolder = Environment.SpecialFolder.MyComputer;
+            folderBrowserDialog.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+
+            //Show the FolderBrowserDialog.
+            DialogResult result = folderBrowserDialog.ShowDialog();
+            if (result != DialogResult.OK)
+            {
+                return;
+            }
+
+            var folderName = folderBrowserDialog.SelectedPath;
+            var dirinfo = new DirectoryInfo(folderName);
+            var allFiles = dirinfo.GetFiles("*", SearchOption.AllDirectories);
+            foreach (FileInfo fileInfo in allFiles)
+            {
+                if (fileInfo.FullName.ToLower().EndsWith(".usfm") ||
+                    fileInfo.FullName.ToLower().EndsWith(".txt"))
+                {
+                    fileDataGrid.Rows.Add(new String[] { fileInfo.FullName });
+                }
+            }
+            //var bindingList = new BindingList<String>(selectedFiles);
+            //var source = new BindingSource(bindingList, null);
+            //fileDataGrid.DataSource = source;
+        }
+
+        private void OnConvertButtonClick(object sender, EventArgs e)
+        {
+            Stream myStream;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.FileName = "out.html";
+            saveFileDialog1.Filter = "HTML files (*.html)|*.html|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 1;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            if ((myStream = saveFileDialog1.OpenFile()) == null)
+            {
+                return;
+            }
+
+            var parser = new USFMToolsSharp.USFMParser();
+            var renderer = new USFMToolsSharp.HtmlRenderer();
+
+            var usfm = new USFMToolsSharp.Models.Markers.USFMDocument();
+            foreach (DataGridViewRow row in fileDataGrid.Rows)
+            {
+                var cell = row.Cells[0];
+                if (cell.Value == null)
+                {
+                    continue;
+                }
+                var filename = cell.Value.ToString();
+                var text = File.ReadAllText(filename);
+                usfm.Contents.AddRange(parser.ParseFromString(text).Contents);
+            }
+            var html = renderer.Render(usfm);
+            var bytes = Encoding.Default.GetBytes(html);
+            myStream.Write(bytes, 0, bytes.Length);
+            myStream.Close();
+
+        }
+
+
+    }
+}
