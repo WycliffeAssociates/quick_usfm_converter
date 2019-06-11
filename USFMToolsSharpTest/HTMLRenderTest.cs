138892689e63cc5963799b178db8b76e1b1b7a10
BryanHo10 <bryankho1010@gmail.com>
2019-06-11 15:59:18 -0400

    added HTMLRender Unit Tests ; targeted NET CORE 1.0

--- USFMToolsSharpTest/HTMLRenderTest.cs
+++ USFMToolsSharpTest/HTMLRenderTest.cs
@@ -19,10 +19,42 @@ namespace USFMToolsSharpTest
         {
             public dynamic expected;
             public dynamic actual;
-            public TestCase(dynamic e, dynamic a)
+            public bool isSingleSpaced;
+            public bool hasOneColumn;
+            public bool isTextJustified;
+            public bool isL2RDirection;
+            public TestCase(dynamic e, dynamic a, bool isSingleSpaced, bool hasOneColumn, bool isTextJustified, bool isL2RDirection)
             {
-                expected = e;
-                actual = a;
+                this.isSingleSpaced = isSingleSpaced;
+                this.hasOneColumn = hasOneColumn;
+                this.isTextJustified = isTextJustified;
+                this.isL2RDirection = isL2RDirection;
+
+
+                StringBuilder output = new StringBuilder();
+                output.AppendLine("<html>");
+                output.AppendLine("<head>");
+                output.AppendLine("<link rel=\"stylesheet\" href=\"style.css\">");
+                output.AppendLine("</head>");
+
+                // HTML tags can only have one class, when render to docx
+                output.AppendLine($"<body class=\"{(isSingleSpaced ? "" : "double-space")}\">");
+                output.AppendLine($"<div class=\"{ (hasOneColumn ? "" : "multi-column")}\">");
+                output.AppendLine($"<div class=\"{ (isTextJustified ? "justified" : "")}\"> ");
+                output.AppendLine($"<div class=\"{ (isL2RDirection ? "" : "rtl-direct")}\"> ");
+
+                output.AppendLine(e);
+
+                output.AppendLine("</div>");
+                output.AppendLine("</div>");
+                output.AppendLine("</div>");
+                output.AppendLine("</div>");
+                output.AppendLine("</body>");
+                output.AppendLine("</html>");
+
+                actual = a.Replace("\r", "").Replace("\n","");
+                expected = output.ToString();
+                expected = expected.Replace("\r", "").Replace("\n", "");
             }
         }
 
@@ -33,24 +65,53 @@ namespace USFMToolsSharpTest
             // RenderMarker(Marker input)
             TestCase[] tests =
             {
-                new TestCase()
+                new TestCase("<div class=\"header\">Genesis</div>",render.Render(parser.ParseFromString("\\h Genesis")),true,true,false,true),
+                new TestCase("<div class=\"header\">1 John</div>",render.Render(parser.ParseFromString("\\h 1 John")),true,true,false,true),
+                new TestCase("<div class=\"header\"></div>",render.Render(parser.ParseFromString("\\h")),true,true,false,true),
+                new TestCase("<div class=\"header\"></div>",render.Render(parser.ParseFromString("\\h      ")),true,true,false,true)
             };
             List<TestCase> TestUSFM = new List<TestCase>(tests);
 
             foreach (TestCase test in TestUSFM)
             {
-                Assert.AreEqual(test.expected.HeaderText, test.actual.HeaderText);
+                Assert.AreEqual(test.expected, test.actual);
             }
         }
         [TestMethod]
         public void TestChapterRender()
         {
+            TestCase[] tests =
+            {
+                new TestCase("<div class=\"chapter\"><span class=\"chaptermarker\">1</span></div>",render.Render(parser.ParseFromString("\\c 1")),true,true,false,true),
+                new TestCase("<div class=\"chapter\"><span class=\"chaptermarker\">1000</span></div>",render.Render(parser.ParseFromString("\\c 1000")),true,true,false,true),
+                new TestCase("<div class=\"chapter\"><span class=\"chaptermarker\">-1</span></div>",render.Render(parser.ParseFromString("\\c -1")),true,true,false,true),
+                new TestCase("<div class=\"chapter\"><span class=\"chaptermarker\">0</span></div>",render.Render(parser.ParseFromString("\\c 0")),true,true,false,true)
+            };
+            List<TestCase> TestUSFM = new List<TestCase>(tests);
 
+            foreach (TestCase test in TestUSFM)
+            {
+                Assert.AreEqual(test.expected, test.actual);
+            }
         }
+
+        // Finish Later
         [TestMethod]
         public void TestVerseRender()
         {
+            TestCase[] tests =
+            {
+                new TestCase("<div class=\"header\">Genesis</div>",render.Render(parser.ParseFromString("\\h Genesis")),true,true,false,true),
+                new TestCase("<div class=\"header\">1 John</div>",render.Render(parser.ParseFromString("\\h 1 John")),true,true,false,true),
+                new TestCase("<div class=\"header\"></div>",render.Render(parser.ParseFromString("\\h")),true,true,false,true),
+                new TestCase("<div class=\"header\"></div>",render.Render(parser.ParseFromString("\\h      ")),true,true,false,true)
+            };
+            List<TestCase> TestUSFM = new List<TestCase>(tests);
 
+            foreach (TestCase test in TestUSFM)
+            {
+                Assert.AreEqual(test.expected, test.actual);
+            }
         }
         [TestMethod]
         public void TestFootnoteRender()
